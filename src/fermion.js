46ab98cd6b1af7b5808700f88c82990b5ed84ebe
Sebastián <sreguera@gmail.com>
2012-02-13 23:02:01 +0100

    Added rotation movement

--- src/fermion.js
+++ src/fermion.js
@@ -14,21 +14,33 @@
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-var craft = {x: 0, y: 0, vx: 0, vy: 0, r: 10, fuel:500, engineOn: false};
-var ground = {x: 0, y:0, width:0, height:0}
+var craft = {x: 0, y: 0, vx: 0, vy: 0,
+             a: 0, turn: 0, 
+             r: 10, 
+             fuel:500, engineOn: false};
+var ground = {x: 0, y:0, width:0, height:0};
 var GRAVITY_ACC = 10;
-var ENGINE_THRUST = 15000;
-var MAX_V = 10;
+var ENGINE_THRUST = 20000;
+var MAX_VY = 10;
+var MAX_VX = 4;
 var TIME_STEP = 0.025;
 var intervalId;
 
 var animate = function() {
+    var ax = 0;
     var ay = GRAVITY_ACC;
+
+    craft.a = craft.a + craft.turn * 0.05;
+
     if (craft.engineOn && craft.fuel > 0) {
-        ay = ay - (ENGINE_THRUST / (500 + craft.fuel));
+        var acc = ENGINE_THRUST / (500 + craft.fuel);
+        ax = ax + acc * Math.sin(craft.a);
+        ay = ay - acc * Math.cos(craft.a);
         craft.fuel = craft.fuel - 1;
     }
+    craft.vx = craft.vx + ax * TIME_STEP;
     craft.vy = craft.vy + ay * TIME_STEP;
+    craft.x = craft.x + craft.vx * TIME_STEP;
     craft.y = craft.y + craft.vy * TIME_STEP;
 }
 
@@ -47,21 +59,29 @@ var drawGround = function(ctx) {
 }
 
 var drawCraft = function(ctx) {
-    var r = craft.r
-    ctx.fillRect(craft.x - r, craft.y - r, 2 * r, 2 * r);
+    ctx.save();
+    ctx.translate(craft.x, craft.y);
+    ctx.rotate(craft.a);
+    var r = craft.r;
+    ctx.fillRect(-r, -r, 2 * r, 2 * r);
     if (craft.engineOn && craft.fuel > 0) {
         ctx.beginPath();
-        ctx.moveTo(craft.x,     craft.y + r);
-        ctx.lineTo(craft.x - r, craft.y + r + 2 * r);
-        ctx.moveTo(craft.x,     craft.y + r);
-        ctx.lineTo(craft.x + r, craft.y + r + 2 * r);
+        ctx.moveTo(0, r);
+        ctx.lineTo(-r, r + 2 * r);
+        ctx.moveTo(0, r);
+        ctx.lineTo(+r, r + 2 * r);
         ctx.stroke();
     }
+    ctx.restore();
 }
 
 var drawHud = function(ctx) {
-    var message = "VY: " + craft.vy.toFixed(1);
-    ctx.fillText(message, 10, 10);
+    var vyMessage = "VY: " + craft.vy.toFixed(1);
+    ctx.fillText(vyMessage, 10, 10);
+    var vxMessage = "VX: " + craft.vx.toFixed(1);
+    ctx.fillText(vxMessage, 10, 30);
+    var degMessage = "DEG: " + ((craft.a * 180) / Math.PI).toFixed(1);
+    ctx.fillText(degMessage, 10, 50);
     var fuelMessage = "FUEL: " + craft.fuel;
     ctx.fillText(fuelMessage, 300, 10);
 }
@@ -90,7 +110,9 @@ var draw = function() {
     if (craftCollide()) {
         clearInterval(intervalId);
         var message;
-        if (craft.vy > MAX_V) {
+        if (craft.vy > MAX_VY 
+            || Math.abs(craft.vx) > MAX_VX
+            || (craft.a / (2 * Math.PI)) > 0.1) {
             message = "CRASH!";
         } else {
             message = "GOOD LANDING!";
@@ -102,10 +124,18 @@ var draw = function() {
     }
 }
 
-var DOWN_ARROW = 40
+var LEFT_ARROW = 37;
+var RIGHT_ARROW = 39;
+var DOWN_ARROW = 40;
 
 var onKeyDown = function(event) {
     switch (event.keyCode) {
+    case LEFT_ARROW:
+        craft.turn = -1;
+        break;
+    case RIGHT_ARROW:
+        craft.turn = +1;
+        break;
     case DOWN_ARROW:
         craft.engineOn = true;
         break;
@@ -114,6 +144,18 @@ var onKeyDown = function(event) {
 
 var onKeyUp = function(event) {
     switch (event.keyCode) {
+    case LEFT_ARROW:
+        if (craft.turn === -1)
+        {
+            craft.turn = 0;
+        }
+        break;
+    case RIGHT_ARROW:
+        if (craft.turn === +1)
+        {
+            craft.turn = 0;
+        }
+        break;
     case DOWN_ARROW:
         craft.engineOn = false;
         break;
